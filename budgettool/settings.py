efa02b08cec151dc0368ce7bb9afe7768afb9f30
Peter Lawson <lawsonpd@gmail.com>
2016-03-12 23:43:57 -0600

    Added ADMINS to settings.

--- budgettool/settings.py
+++ budgettool/settings.py
@@ -28,6 +28,8 @@ with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+ADMINS = [('Peter', 'lawsonpd@gmail.com')]
+
 ALLOWED_HOSTS = [
     'localhost',
     'tipout-app.herokuapp.com',
