0404c3ecfe639e3b9034a0519bc79b5c32856098
Peter Lawson <lawsonpd@gmail.com>
2017-04-11 16:03:42 -0500

    Add Stripe plans to subscription.py.

--- tipout/subscription.py
+++ tipout/subscription.py
@@ -2,10 +2,18 @@ import stripe
 
 stripe.api_key = 'sk_test_GVCFYxcWaAZq3zBnEifLXeJd'
 
-plan = stripe.Plan.create(
-    name='Basic Plan',
-    id='basic-monthly',
+basic_plan = stripe.Plan.create(
+    name='Free Plan',
+    id='free-plan',
     interval='month',
     currency='usd',
-    amount=0
+    amount=0,
+)
+
+premium_plan = stripe.Plan.create(
+    name='Paid Plan',
+    id='paid-plan',
+    interval='month',
+    currency='usd',
+    amount='500',
 )
