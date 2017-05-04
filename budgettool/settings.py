1dbec0360f3f42635740f15867ea9df6c1a3d882
Peter Lawson <lawsonpd@gmail.com>
2017-05-04 11:27:24 -0500

    Connecting to memcache with memcachier, pylibmc, django-pylibmc.

--- budgettool/settings.py
+++ budgettool/settings.py
@@ -172,14 +172,38 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 LOGIN_REDIRECT_URL = '/budget/'
 
 # Memcached
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
-    }
-}
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+#         'LOCATION': '127.0.0.1:11211',
+#     }
+# }
 
 SESSIONS_ENGINE = 'django.contrib.sessions.backends.cached_db'
+
+# from https://devcenter.heroku.com/articles/django-memcache#further-reading-and-resources
+def get_cache():
+    import os
+    try:
+        os.environ['MEMCACHE_SERVERS'] = os.environ['MEMCACHIER_SERVERS'].replace(',', ';')
+        os.environ['MEMCACHE_USERNAME'] = os.environ['MEMCACHIER_USERNAME']
+        os.environ['MEMCACHE_PASSWORD'] = os.environ['MEMCACHIER_PASSWORD']
+        return {
+            'default': {
+                'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
+                'TIMEOUT': 500,
+                'BINARY': True,
+                'OPTIONS': { 'tcp_nodelay': True }
+            }
+        }
+    except:
+        return {
+            'default': {
+                'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
+            }
+        }
+
+CACHES = get_cache()
 
 # CACHE_MIDDLEWARE_ALIAS
 
