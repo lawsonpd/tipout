7326014d712fcbc62eb15be557f8e56d6bdc3ec9
Peter Lawson <lawsonpd@gmail.com>
2017-05-04 16:35:34 -0500

    Cache tip data and add update_budgets to tips.py.

--- tipout/tips.py
+++ tipout/tips.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import permission_required, login_required
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods
-from django.utils.timezone import now
+from django.utils.timezone import now, timedelta
 from django.core.cache import cache
 from django.views.decorators.cache import cache_control
 
@@ -16,8 +16,9 @@ from budget_utils import (avg_daily_tips_earned,
                           update_budgets
                          )
 
+@cache_control(private=True)
 @login_required(login_url='/login/')
-@permission_required('tipout.use_tips', login_url='/signup/')
+@require_http_methods(['GET', 'POST'])
 def enter_tips(request):
     # if this is a POST request, we need to process the form data
     if request.method == 'POST':
@@ -56,10 +57,15 @@ def tips(request):
         u = TipoutUser.objects.get(email=request.user)
         emp = Employee.objects.get(user=u)
         t = now().date()
+
+        tips = cache.get('tips')
+        if not tips:
+            tips = Tip.objects.filter(owner=emp)
+            cache.set('tips', tips)
+
         # tips = Tip.objects.filter(owner=emp,
         #                           date_earned__month=now().date().month).order_by('date_earned')[::-1]
         if (now().date() - emp.signup_date).days <= 30:
-            tips = Tip.objects.filter(owner=emp).order_by('-date_earned')
             tip_values = [ tip.amount for tip in tips ]
             avg_daily_tips = pretty_dollar_amount(avg_daily_tips_earned_initial(emp.init_avg_daily_tips, tip_values, emp.signup_date))
             return render(request, 'tips.html', {'avg_daily_tips': avg_daily_tips,
@@ -68,9 +74,8 @@ def tips(request):
                                                 }
                          )
         else:
-            tips = Tip.objects.filter(owner=emp,
-                                      date_earned__month=now().date().month).order_by('-date_earned')
-            tip_values = [ tip.amount for tip in tips ]
+            recent_tips = tips.filter(date_earned__gte=(now().date()-timedelta(30)))
+            tip_values = [ tip.amount for tip in recent_tips ]
             avg_daily_tips = pretty_dollar_amount(avg_daily_tips_earned(tip_values))
             return render(request, 'tips.html', {'avg_daily_tips': avg_daily_tips,
                                                  'tips': tips,
@@ -78,24 +83,39 @@ def tips(request):
                                                 }
                          )
 
+@cache_control(private=True)
 @login_required(login_url='/login/')
 def edit_tip(request, *args):
     pass
 
+@cache_control(private=True)
 @login_required(login_url='/login/')
+@require_http_methods(['POST'])
 def delete_tip(request, tip_id, *args):
 
     if request.method == 'POST':
         u = TipoutUser.objects.get(email=request.user)
         emp = Employee.objects.get(user=u)
 
-        t = Tip.objects.get(owner=emp, pk=tip_id)
+        tips = cache.get('tips')
+        if not tips:
+            tips = Tip.objects.filter(owner=emp)
+            cache.set('tips', tips)
+
+        t = tips.get(owner=emp, pk=tip_id)
         t.delete()
+
+        tips = Tip.objects.filter(owner=emp)
+        cache.set('tips', tips)
+
         if t.date_earned < now().date():
             update_budgets(emp, t.date_earned)
+
         return redirect('/tips/')
 
+@cache_control(private=True)
 @login_required(login_url='/login/')
+@require_http_methods(['GET'])
 def tips_archive(request, year=None, month=None, day=None, *args):
     '''
     See past tips.
@@ -107,31 +127,33 @@ def tips_archive(request, year=None, month=None, day=None, *args):
     u = TipoutUser.objects.get(email=request.user)
     emp = Employee.objects.get(user=u)
 
+    tips = cache.get('tips')
+    if not tips:
+        tips = Tip.objects.filter(owner=emp)
+        cache.set('tips', tips)
+
     if not year:
-        all_years = [tip.date_earned.year for tip in Tip.objects.filter(owner=emp)]
+        all_years = [tip.date_earned.year for tip in tips]
         years = set(all_years)
         return render(request, 'tips_archive.html', {'years': years})
 
     elif not month:
-        dates = Tip.objects.filter(owner=emp,
-                                   date_earned__year=year).dates('date_earned', 'month')
+        dates = tips.filter(date_earned__year=year).dates('date_earned', 'month')
         months = [date.month for date in dates]
         return render(request, 'tips_archive.html', {'year': year,
                                                      'months': months})
     elif not day:
-        dates = Tip.objects.filter(owner=emp,
-                                   date_earned__year=year,
-                                   date_earned__month=month).dates('date_earned', 'day')
+        dates = tips.filter(date_earned__year=year,
+                            date_earned__month=month).dates('date_earned', 'day')
         days = [date.day for date in dates]
         return render(request, 'tips_archive.html', {'year': year,
                                                      'month': month,
                                                      'days': days})
     else:
-        tips = Tip.objects.filter(owner=emp,
-                                  date_earned__year=year,
-                                  date_earned__month=month,
-                                  date_earned__day=day)
+        tips_for_day = tips.filter(date_earned__year=year,
+                                   date_earned__month=month,
+                                   date_earned__day=day)
         return render(request, 'tips_archive.html', {'year': year,
                                                      'month': month,
                                                      'day': day,
-                                                     'tips': tips})
+                                                     'tips': tips_for_day})
